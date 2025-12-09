c018e76ecab391e6d5902b949eda567ddee1e15a
dvieira13 <vieiradylan13@gmail.com>
2025-12-09 14:04:26 -0500

    Prepping for deployment

--- api/backend.ts
+++ api/backend.ts
@@ -1,8 +1,6 @@
 // api/backend.ts
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../server/src/app";
 
-export default (req: VercelRequest, res: VercelResponse) => {
-    // Express app is just a (req, res) handler
-    return app(req as any, res as any);
-};
+export default function handler(req: any, res: any) {
+    return app(req, res);
+}
